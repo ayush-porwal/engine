03106f441d8ec245ae32cac275a7e667baade7b8
Ayush Porwal <ayush-porwal@lxuser-HP-ProBook-450-15-6-inch-G10-Notebook-PC>
2025-04-01 15:44:53 +0200

    Initial commit
    To connect your workspace to Nx Cloud, push your repository
    to your git hosting provider and go to the following URL:
    
    https://cloud.nx.app/connect/Gw2279Sytk

---
+++ apps/engine-auth/jest.config.ts
@@ -0,0 +1,10 @@
+export default {
+  displayName: 'engine-auth',
+  preset: '../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/engine-auth',
+};
